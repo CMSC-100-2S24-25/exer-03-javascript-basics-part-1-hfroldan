0a5d384df3566fba21f5742af2a6ad8ee8bc6f6f
hfroldan <hfroldan@up.edu.ph>
2025-02-18 14:41:04 +0800

    feat: Add validation function

---
+++ index.js
@@ -0,0 +1,17 @@
+function validatePassword(pswd1, pswd2) {
+    if (pswd1 !== pswd2) return false
+
+    if (String(pswd1).length < 8 || String(pswd2).length < 8) return false
+
+    var number_ctr = 0, upper_ctr = 0, lower_ctr = 0;
+
+    for (let char of pswd1) {
+        if ("1234567890".includes(char)) number_ctr++
+        if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(char)) upper_ctr++
+        if ("abcdefghijklmnopqrstuvwxyz".includes(char)) lower_ctr++
+
+        if (number_ctr > 0 && upper_ctr > 0 && lower_ctr > 0) return true
+    }
+
+    return false
+}
